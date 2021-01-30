96e1113436588ab6c2e40d666a5d6f9c9fd76a4d
Bratislav Ljubisic <bratislav.ljubisic@gmail.com>
2021-01-30 15:15:17 +0100

    Initial Commit

---
+++ SimpleBox/SimpleBoxApp.swift
@@ -0,0 +1,20 @@
+//
+//  SimpleBoxApp.swift
+//  SimpleBox
+//
+//  Created by Bratislav Ljubisic on 30.01.21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBoxApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
